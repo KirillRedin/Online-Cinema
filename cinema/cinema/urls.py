20ce06d985863887080860de0da86dd786956368
Kirill Redin <redinkirill.ua@gmail.com>
2018-05-15 18:23:22 +0300

    run error fixed

--- cinema/cinema/urls.py
+++ cinema/cinema/urls.py
@@ -21,5 +21,5 @@ from rest_framework import routers
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', TemplateView.as_view(template_name="index.html")),
-    url(r'^order', include(''))
+    # url(r'^order', include(''))
 ]
